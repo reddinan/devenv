9fbaa1aeadc6eb886b01c7868b3b9c926edb5e20
Andrew Reddin <andrew@bytepath.ca>
2025-03-23 16:00:23 -0300

    now installs valet I think

--- install.sh
+++ install.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-#apt-get update -y
-#apt-get upgrade -y
+apt-get update -y
+apt-get upgrade -y
 
-#echo "Installing ansible"
-apt-get install -y python3 ansible git
+# Parse the command line arguments
+while getopts "u:" opt; do
+    case "$opt" in
+    u)  USERNAME=$OPTARG
+        ;;
+    esac
+done
+
+echo "user name is " $USERNAME
+
+if [ -z "$USERNAME" ]; then
+  echo "No username specified so using 'andrew'"
+  echo "specify a different user with -u username"
+  USERNAME=andrew
+else
+  echo "Installing for user " $USERNAME 
+fi
+
+echo "Installing ansible"
+apt-get install -y python3 ansible git sudo python3-pexpect
 
 # Fetch all of the directories in the provision/migrations directory
 for dir in `ls -d Installables/*/ | sort` ; do
@@ -14,7 +32,7 @@ for dir in `ls -d Installables/*/ | sort` ; do
 
 	for FILE in `find Installables/$NAME -type f | sort`;do
 	echo '######'$FILE
-	ansible-playbook  --connection=local --inventory 127.0.0.1, $FILE
+	ansible-playbook  --connection=local --inventory 127.0.0.1, -e "username=$USERNAME" $FILE --extra-vars "ansible_sudo_pass=password"
 	done
 	#export ANSIBLE_HOST_KEY_CHECKING=False;
 	#ansible-playbook  -u root -i "$2," --private-key $1 -e "migration_name=$NAME domain=$3" migrations/install_provision.yml;
